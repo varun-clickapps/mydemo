11cd57f0755c828acb922693b7ef75552a1d3c05
Varun <varun@clickapps.co>
2016-04-27 13:19:46 +0530

    add server model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+root 'servers#index'  
+resources :servers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
